546d95b1d5e2bdad841a368861fe8f96a41bdc2a
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-11-10 15:15:30 +0100

    Support running benchmarks for multiple SHAs.

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# $1 - BENCHMARK_DIR
+# $2 - BOOST_ROOT
+
+for f in `cat sha`; do
+
+    echo "SHA: $f"
+
+    # checkout the Boost.Geometry commit
+    cd $2/libs/geometry && git checkout $f
+    # update headers
+    cd $2 && ./b2 headers
+
+    # ./run.sh BOOST_ROOT TIMESTAMP SHA
+    cd $1 && ./run.sh $2 `cd $2/libs/geometry && date -d"\`git show --quiet --format="%ci"\`" --utc +%Y.%m.%d-%H:%M:%S` $f
+
+done
+
