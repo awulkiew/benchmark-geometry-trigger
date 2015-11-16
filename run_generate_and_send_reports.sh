e454210930a519c11f314d37abb21d4aa3a2e16f
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2015-11-16 21:57:07 +0100

    Retry to push results on failure caused by a different push done during the benchmark run.

---
+++ run_generate_and_send_reports.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# $1 - BENCHMARK_ROOT
+# $2 - BOOST_ROOT
+
+git config --global user.name "awulkiew-machine"
+git config --global user.email "awulkiew.machine@gmail.com"
+git config --global push.default simple
+
+for i in `seq 1 5`; do
+
+    # generate reports
+    for f in `cat sha`; do
+        # generate report
+        cd $1 && ./run_report.sh `cd $2/libs/geometry && date -d"\`git show --quiet --format="%ci"\`" --utc +%Y.%m.%d-%H:%M:%S` $f
+    done
+
+    echo "Push the reports"
+    cd $1 && git add results
+    cd $1 && git commit -m "`cat $PROJECT_ROOT/sha`"
+    cd $1 && git push
+
+    if [ $? -eq 0 ] then
+        echo "OK"
+        exit 0
+    elif [ $i -le 5 ] then
+        echo "Error. Cleanup and try again."
+        cd $1 && git reset HEAD^
+        cd $1 && rm -Rf results/*
+        cd $1 && git checkout results
+    fi
+
+done
+
+exit 1
