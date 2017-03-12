ec3f2d12d1dc68f2593c9037c6d1fc76433d79dc
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2017-03-12 01:15:03 +0100

    Copy to CircleCI artifacts recursively.

--- run_generate_and_send_reports.sh
+++ run_generate_and_send_reports.sh
@@ -13,9 +13,6 @@ status=1
 
 for i in `seq 1 5`; do
 
-    pwd
-    ls -lah
-
     echo "Generate reports"
     for f in `cat $PROJECT_ROOT/sha`; do
         cd $1 && ./run_report.sh `cd $2/libs/geometry && date -d"\`git show --quiet --format="%ci"\`" --utc +%Y.%m.%d-%H:%M:%S` $f $3
@@ -49,10 +46,10 @@ done
 
 # generate artifacts for logging purposes
 mkdir -p $CIRCLE_ARTIFACTS/results
-cp $3/* $CIRCLE_ARTIFACTS/results/
+cp -r $3/* $CIRCLE_ARTIFACTS/results/
 mkdir -p $CIRCLE_ARTIFACTS/gh-pages
-cp $4/* $CIRCLE_ARTIFACTS/gh-pages/
+cp -r $4/* $CIRCLE_ARTIFACTS/gh-pages/
 mkdir -p $CIRCLE_ARTIFACTS/temp
-cp $BENCHMARK_ROOT/temp/* $CIRCLE_ARTIFACTS/temp/
+cp -r $BENCHMARK_ROOT/temp/* $CIRCLE_ARTIFACTS/temp/
 
 exit $status
